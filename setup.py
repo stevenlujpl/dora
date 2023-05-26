c2f09c732bb49471410eebf30fe0cc8553b3055e
Umaa Rebbapragada <urebbapr@jpl.nasa.gov>
2023-05-26 16:57:47 -0700

    commenting out tensorflow, scikit-image

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
         'matplotlib==3.3.4',
         'numpy==1.19.5',
         'scipy==1.5.4',
-        'scikit-image==0.17.2',
+#        'scikit-image==0.17.2',
         'scikit-learn==0.24.2',
         'Pillow==8.2.0',
         'planetaryimage==0.5.0',
@@ -22,9 +22,9 @@ setup(
         'pandas==1.1.5',
         'rasterio==1.2.6',
         'tqdm==4.62.0',
-        'tensorflow==2.5.1',
-        'tensorflow-probability==0.13.0',
-        'tensorflow-addons==0.13.0',
+#        'tensorflow==2.5.1',
+#        'tensorflow-probability==0.13.0',
+#        'tensorflow-addons==0.13.0',
         'sklearn-som==1.1.0'
     ],
     provide=[
