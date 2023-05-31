640c225bdb0c5e6a6b2b9f674997dc6592a8c03a
Umaa Rebbapragada <urebbapr@jpl.nasa.gov>
2023-05-31 13:17:58 -0700

    commented PAE registration to avoid tensorflow imports

--- dora_exp_pipeline/dora_exp.py
+++ dora_exp_pipeline/dora_exp.py
@@ -23,7 +23,7 @@ from dora_exp_pipeline.rx_outlier_detection import RXOutlierDetection
 from dora_exp_pipeline.random_outlier_detection import RandomOutlierDetection
 from dora_exp_pipeline.negative_sampling_outlier_detection import \
     NegativeSamplingOutlierDetection
-from dora_exp_pipeline.pae_outlier_detection import PAEOutlierDetection
+#from dora_exp_pipeline.pae_outlier_detection import PAEOutlierDetection
 from dora_exp_pipeline.util import LogUtil
 from dora_exp_pipeline.dora_feature import extract_feature
 from dora_exp_pipeline.dora_feature import z_score_normalize
@@ -59,9 +59,9 @@ def register_od_algs():
     negative_sampling_outlier_detection = NegativeSamplingOutlierDetection()
     register_od_alg(negative_sampling_outlier_detection)
 
-    # Register PAE outlier detection algorithm in the pool
-    pae_outlier_detection = PAEOutlierDetection()
-    register_od_alg(pae_outlier_detection)
+#    # Register PAE outlier detection algorithm in the pool
+#    pae_outlier_detection = PAEOutlierDetection()
+#    register_od_alg(pae_outlier_detection)
 
 
 def start(config_file: str, out_dir: str, log_file=None, seed=1234):
